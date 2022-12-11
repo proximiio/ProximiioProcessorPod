6b47a36dd22731c6d1c5e1c908afc51ef6a0835f
Matteo Crippa <matteocrippa@users.noreply.github.com>
2022-12-12 00:10:27 +0100

    added xcframework

---
+++ ProximiioProcessor.xcframework/ios-arm64_x86_64-simulator/ProximiioProcessor.framework/Headers/processor.h
@@ -0,0 +1,19 @@
+//
+//  processor.h
+//  processor
+//
+//  Created by dev on 2/6/20.
+//  Copyright © 2020 Proximi.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for processor.
+FOUNDATION_EXPORT double processorVersionNumber;
+
+//! Project version string for processor.
+FOUNDATION_EXPORT const unsigned char processorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <processor/PublicHeader.h>
+
+
